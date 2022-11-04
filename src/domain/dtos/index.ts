c3b0e89091149f53fba84ff7a2f3e88298ba2629
Alejandra Joaquin <ajoaquin@mulesoft.com>
2022-11-04 02:04:11 -0300

    fixing login

--- src/domain/dtos/index.ts
+++ src/domain/dtos/index.ts
@@ -1,5 +1,6 @@
 export * from './comment.request';
 export * from './course.search.request';
 export * from './enroll.request';
+export * from './login.request';
 export * from './notification.request';
 export * from './update.inscription.request';
--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 import { UsersService } from '../users/users.service';
 
 @Injectable()
@@ -9,9 +10,9 @@ export class AuthService {
         private jwtService: JwtService,
     ) {}
 
-    async validateUser(email: string, pass: string): Promise<any> {
-        const user = await this.usersService.fineByEmail(email);
-        if (user && user.password === pass) {
+    async validateUser(username: string, pass: string): Promise<any> {
+        const user = await this.usersService.fineByEmail(username);
+        if (bcrypt.compareSync(pass, user.password)) {
             const { password, ...result } = user;
             return result;
         }
@@ -19,7 +20,7 @@ export class AuthService {
     }
 
     async login(user: any) {
-        const payload = { username: user.email, sub: user.userId };
+        const payload = { username: user.email, sub: user.id };
         return {
             access_token: this.jwtService.sign(payload),
         };
