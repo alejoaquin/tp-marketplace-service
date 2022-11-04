cbfd051f3506a060efb2561fbcdc5633a7a30fbe
Alejandra Joaquin <ajoaquin@mulesoft.com>
2022-11-04 14:57:31 -0300

    public endpoints set up

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { Public } from 'src/public.decorator';
 import { AuthService } from 'src/services/auth/auth.service';
 import { LocalAuthGuard } from 'src/services/auth/local-auth.guard';
@@ -9,13 +9,8 @@ export class AppController {
 
     @Public()
     @UseGuards(LocalAuthGuard)
-    @Post('auth/login')
+    @Post('login')
     async login(@Request() req) {
         return this.authService.login(req.user);
-    }
-
-    @Get('profile')
-    getProfile(@Request() req) {
-        return req.user;
     }
 }
--- src/controllers/courses.controller.ts
+++ src/controllers/courses.controller.ts
@@ -17,6 +17,7 @@ import {
     InscriptionEntity,
     UpdateInscriptionRequest,
 } from 'src/domain';
+import { Public } from 'src/public.decorator';
 import { CommentsService } from 'src/services/comments/comments.service';
 import { CoursesService } from 'src/services/courses/courses.service';
 import { InscriptionsService } from 'src/services/inscriptions/inscriptions.service';
@@ -29,6 +30,7 @@ export class CoursesController {
         private inscriptionsService: InscriptionsService,
     ) {}
 
+    @Public()
     @Get()
     async getAll(): Promise<CourseEntity[]> {
         return this.coursesService.getAll();
